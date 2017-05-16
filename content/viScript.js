6d3fc5b7bf26dd7baf829aae4a2d827ec27843ee
veysel <vinanc72@gmail.com>
2017-05-16 23:35:49 +0300

    Custom script code review

--- content/viScript.js
+++ content/viScript.js
@@ -1,18 +1,21 @@
+var Tools = {
+    AddName: function (tempName) {
+        var newTag = $("<a></a>").text(tempName).addClass("list-group-item list-title");
+        $(".list-group").append(newTag);
+    }
+}
+
 if (localStorage.getItem("viContent")) {
     var viObject = JSON.parse(localStorage.getItem("viContent"));
-    var test = $("<a></a>").text(viObject.name).addClass("list-group-item list-title");
-    $(".list-group").append(test);
-    console.log(viObject);
+    Tools.AddName(viObject.name);
 }
 else {
     $.ajax({
         url: "https://api.github.com/users/veysel",
         success: function (result) {
             var viObject = JSON.stringify({ name: result.name });
-            var test = $("<a></a>").text(result.name).addClass("list-group-item list-title");
-            $(".list-group").append(test);
+            Tools.AddName(viObject.name);
             localStorage.setItem("viContent", viObject);
-            console.log(viObject);
         }
     });
 }
